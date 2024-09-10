9db9880bcaf0bb62746d778dbdf5790fe7921ca0
dePaula06 <p.depaula06.sp@gmail.com>
2024-09-10 15:51:04 -0300

    import new components/pages and create news routes

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,17 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 //Import das páginas para navegação
 import Index from "./web";
+import Error from "./web/error";
+import Login from "./web/login";
+import Register from "./web/register";
 
 //Import de components
 import HeaderIndex from "./components/index/header";
+import InputEmail from "./components/inputs/inputEmail";
+import InputPassword from "./components/inputs/inputPassword";
+import InputCPF from "./components/inputs/inputCPF";
+import InputName from "./components/inputs/inputName";
+import InputPhone from "./components/inputs/inputPhone";
 
 function LayoutIndex() {
   <div>
@@ -20,6 +28,9 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
+          <Route path="/error" element={<Error />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
         </Routes>
       </BrowserRouter>
     </div>
