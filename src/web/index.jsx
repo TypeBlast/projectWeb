2a5f9e8e0feea44b6da0af038871a2b848a272d7
dePaula06 <p.depaula06.sp@gmail.com>
2024-09-10 21:24:56 -0300

    feat: adding onboarding

--- src/web/index.jsx
+++ src/web/index.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+
 //import components
 import Header from "../components/index/header";
 
@@ -7,12 +8,17 @@ import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 
+
 import { useMediaQuery } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 
-//Import images
+//import images
 import ImageHome from "../assets/images/imageHome.png";
-import ImageProducts from "../assets/images/imageProducts.png";
+import ImageProducts from "../assets/images/imageProducts.png"
+import ImageServices from "../assets/images/imageServices.png"
+import ImageConsults from "../assets/images/imageConsults.png"
+
+
 
 function Index() {
   // Hook para verificar o tamanho da tela
@@ -23,17 +29,17 @@ function Index() {
     navigate(path); // Chama o hook de navegação
   };
   return (
-    <div>
+    <div style={{paddingBottom: 50}}>
       <Header />
       <Grid container>
-        <Grid item xs={12} md={6}>
-          <Typography className="titleWelcome">
-            Tudo o que Seu Pet Precisa em um Só Lugar!
-          </Typography>
-          <Typography className="textWelcome">
-            Produtos, Serviços e Consultas para Cuidar e Mimá-lo com Qualidade e
-            Amor
-          </Typography>
+        <Grid item xs={12} md={6}
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          flexDirection: 'column'
+        }}>
+          <Typography className="titleWelcome">Tudo o que Seu Pet Precisa em um Só Lugar!</Typography>
+          <Typography className="textWelcome">Produtos, Serviços e Consultas para Cuidar e Mimá-lo com Qualidade e Amor</Typography>
           <Button className="buttonComecar" onClick={() => handleNavigation("/login")}>Começar</Button>
         </Grid>
 
@@ -52,91 +58,102 @@ function Index() {
               style={{
                 width: 450,
                 height: 400,
-                marginTop: 10,
+                marginTop: '10%'
               }}
               alt="Home"
             />
           </Grid>
         )}
       </Grid>
-      <Grid container sx={{ marginTop: 20 }}>
+      <Grid container sx={{marginTop: 20}}>
         {!isMediumScreen && (
-          <Grid
-            item
-            md={6}
-            sx={{
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "center",
-            }}
-          >
+          <Grid item md={6} 
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}>
             <img
               src={ImageProducts}
               style={{
                 width: 350,
-                height: 300,
-                marginTop: 10,
+                height: 300 ,
+                marginTop: '5%',
               }}
-              alt="Home"
+              alt="Produtos"
             />
           </Grid>
         )}
-        <Grid
-          item
-          xs={12}
-          md={6}
-          sx={{
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-            flexDirection: "column",
-          }}
-        >
-          <Typography className="titleOnboarding">Produtos</Typography>
-          <Typography className="textOnboarding">
-            Encontre tudo que seu pet precisa para ser feliz e saudável aqui no
-            nosso app!
-          </Typography>
+        <Grid item xs={12} md={6}
+         sx={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          flexDirection: 'column'
+        }}>
+        <Typography className="titleOnboarding">Produtos</Typography>
+        <Typography className="textOnboarding">Encontre tudo que seu pet precisa para ser feliz e saudável aqui no nosso app!</Typography>
         </Grid>
       </Grid>
-      <Grid container sx={{ marginTop: 20 }}>
+      <Grid container sx={{marginTop: 20}}> 
+        <Grid item xs={12} md={6}
+         sx={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          flexDirection: 'column'
+        }}>
+        <Typography className="titleOnboarding">Servicos</Typography>
+        <Typography className="textOnboarding">Transforme o visual do seu pet com nossos serviços de banho e tosa, garantindo cuidado e carinho em cada detalhe.</Typography>
+        </Grid>
         {!isMediumScreen && (
-          <Grid
-            item
-            xs={12}
-            md={6}
-            sx={{
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "center",
-              flexDirection: "column",
-            }}
-          >
-            <Typography className="titleOnboarding">Produtos</Typography>
-            <Typography className="textOnboarding">
-              Encontre tudo que seu pet precisa para ser feliz e saudável aqui
-              no nosso app!
-            </Typography>
-          </Grid>
-        )}
-        <Grid
-          item
-          md={6}
+        <Grid item md={6} 
           sx={{
             display: "flex",
             alignItems: "center",
             justifyContent: "center",
-          }}
-        >
-          <img
-            src={ImageProducts}
-            style={{
-              width: 350,
-              height: 300,
-              marginTop: 10,
-            }}
-            alt="Home"
-          />
+            textAlign: 'center' 
+          }}>
+            <img
+              src={ImageServices}
+              style={{
+                width: 380,
+                height: 350,
+                marginTop: '5%',
+              }}
+              alt="Serviços"
+            />
+          </Grid>
+          )}
+      </Grid>
+      <Grid container sx={{marginTop: 20}}>
+        {!isMediumScreen && (
+          <Grid item md={6} 
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}>
+            <img
+              src={ImageConsults}
+              style={{
+                width: 390,
+                height: 380 ,
+                marginTop: '5%',
+              }}
+              alt="Consults"
+            />
+          </Grid>
+        )}
+        <Grid item xs={12} md={6}
+         sx={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          flexDirection: 'column'
+        }}>
+        <Typography className="titleOnboarding">Consultas</Typography>
+        <Typography className="textOnboarding">Cuide da saúde do seu pet com nossas consultas veterinárias, oferecendo atenção especializada e bem-estar em cada visita.</Typography>
         </Grid>
       </Grid>
     </div>
