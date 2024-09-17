4576cc42b5e0e541c416657ef0a0c1b2d6e333f3
dePaula06 <p.depaula06.sp@gmail.com>
2024-09-17 10:07:12 -0300

    feat: Creation and initial configuration of products, services and user pages

---
+++ src/web/products.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Typography, Button} from "@mui/material";
+
+function Products() {
+
+  return (
+    <div className="container">
+      
+    </div>
+  );
+}
+
+export default Products;
---
+++ src/web/services.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Typography, Button} from "@mui/material";
+
+function Services() {
+
+  return (
+    <div className="container">
+      
+    </div>
+  );
+}
+
+export default Services;
