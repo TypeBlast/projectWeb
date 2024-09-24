a834ebe34077f57cf73f2a88fc7078563c44e406
Vinicius <vinicius.souza.oliveira123@gmail.com>
2024-09-24 13:04:04 -0300

    feat: display of products pulled from db

--- src/axios/axios.js
+++ src/axios/axios.js
@@ -1,21 +1,31 @@
-import axios from "axios"
+import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:5000/api/",
-    timeout: 10000, // Tempo de espera máximo
-    headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json' // Certifique-se de que este cabeçalho esteja presente
-    },
-  });
+  baseURL: "http://localhost:5000/api/",
+  timeout: 10000, // Tempo de espera máximo
+  headers: {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json', // Certifique-se de que este cabeçalho esteja presente
+  },
+});
 
 const sheets = {
-    logUser: (user) => api.post("/auth/login", user),
-    postUser: (user) => api.post("/user/", user),
-    putUser: (user, id) => api.put(`/user/${id}`, user),
-    deleteUser: (id) => api.delete(`/user/${id}`),
-    getUser: (id) => api.get(`/user/${id}`),
-    getAllUsers: () => api.get("/user"),
-}
+  // Rotas de usuários
+  logUser: (user) => api.post("/auth/login", user),
+  postUser: (user) => api.post("/user/", user),
+  putUser: (user, id) => api.put(`/user/${id}`, user),
+  deleteUser: (id) => api.delete(`/user/${id}`),
+  getUser: (id) => api.get(`/user/${id}`),
+  getAllUsers: () => api.get("/user"),
+
+  // Rotas de produtos
+  createProduct: (product) => api.post("/products", product),
+  getAllProducts: () => api.get("/products"),
+  getProductById: (id) => api.get(`/products/${id}`),
+  updateProduct: (id, product) => api.put(`/products/${id}`, product),
+  deleteProduct: (id) => api.delete(`/products/${id}`),
+  getProductsByCategory: (category_id) => api.get(`/products/category/${category_id}`),
+  getProductsByName: (name) => api.get(`/products/search/${name}`),
+};
 
 export default sheets;
