f7306609818409de31e4c435fd1694bdf057c109
dePaula06 <p.depaula06.sp@gmail.com>
2024-09-25 21:49:48 -0300

    chore: New searchBar styling (now displays all buttons internally)

--- src/components/layout/searchBar.jsx
+++ src/components/layout/searchBar.jsx
@@ -1,5 +1,11 @@
 import React, { useState } from "react";
-import { TextField, Box, InputAdornment, IconButton, Button } from "@mui/material";
+import {
+  TextField,
+  Box,
+  InputAdornment,
+  IconButton,
+  Button,
+} from "@mui/material";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
 import CloseIcon from "@mui/icons-material/Close";
@@ -15,45 +21,66 @@ function SearchBar({ onSearch }) {
   const handleSearch = () => {
     onSearch(value); // Chama a função de pesquisa com o valor atual do input
   };
-  
+
   return (
-    <Box sx={{ display: "flex", justifyContent: "center", width: "100%" }}>
+    <Box sx={{ display: "flex", justifyContent: "center", width: "100%", alignItems: "center" }}>
       <TextField
         id="outlined-search"
-        label="O que seu pet precisa?"
         value={value}
         onChange={(e) => setValue(e.target.value)} // Atualiza o valor do campo
         InputProps={{
           startAdornment: (
-            <InputAdornment position="start">
-              <FontAwesomeIcon
-                icon={faSearch}
-                style={{ fontSize: "20px", color: "#BFBFBF" }}
-              />
+            <InputAdornment position="start" sx={{ marginRight: 0 }}>
+              {value === "" && (
+                <FontAwesomeIcon
+                  icon={faSearch}
+                  style={{ fontSize: "20px", color: "#BFBFBF", marginLeft: "25px" }}
+                />
+              )}
             </InputAdornment>
           ),
           endAdornment: (
             <InputAdornment position="end">
               {value && (
-                <IconButton onClick={handleClear} size="small">
-                  <CloseIcon style={{ color: "#BFBFBF" }} />
-                </IconButton>
+                <>
+                  <IconButton onClick={handleClear} size="small">
+                    <CloseIcon style={{ color: "#BFBFBF" }} />
+                  </IconButton>
+                  <Button
+                    onClick={handleSearch}
+                  >
+                    <FontAwesomeIcon
+                  icon={faSearch}
+                  style={{ fontSize: "20px", color: "#BFBFBF", marginLeft: "25px" }}
+                />
+                  </Button>
+                </>
               )}
             </InputAdornment>
           ),
-          style: { paddingLeft: "30px" },
+          style: { paddingLeft: 0 },
         }}
-        type="search"
+        placeholder="O que seu pet precisa?" // Placeholder com lupa e texto
+        type="text"
         variant="outlined"
-        sx={{ width: "80vw", maxWidth: "800px" }}
+        sx={{
+          width: "70vw",
+          maxWidth: "800px",
+          borderRadius: "10px",
+          "& .MuiInputBase-input": {
+            fontFamily: "Poppins-Regular", // Fonte para o texto digitado
+            fontSize: "1.2rem",
+            color: "#333", // Cor do texto da pesquisa
+            marginLeft: "25px"
+          },
+          "& .MuiInputLabel-root": {
+            fontFamily: "Poppins-Regular", // Fonte para o placeholder
+            fontSize: "16px",
+            color: "#BFBFBF", // Cor do placeholder
+            marginLeft: "25px"
+          },
+        }}
       />
-      <Button
-        variant="contained"
-        onClick={handleSearch} // Chama a pesquisa ao clicar
-        sx={{ marginLeft: "10px", backgroundColor: "#BFBFBF" }}
-      >
-        Pesquisar
-      </Button>
     </Box>
   );
 }
