21d3045e2aa50476b2f42515056c4f7db683ee1b
dePaula06 <p.depaula06.sp@gmail.com>
2024-09-30 11:52:10 -0300

    chore(home): Carousels responsiveness

--- src/web/home.jsx
+++ src/web/home.jsx
@@ -1,21 +1,28 @@
 import React from "react";
-
-import { Typography, Button, Box } from "@mui/material";
+import { Typography, Button, Box, useMediaQuery } from "@mui/material"; // Importa useMediaQuery
 
 import SearchBar from "../components/layout/searchBar";
 import BoxWelcome from "../components/layout/boxWelcome";
 import Categories from "../components/layout/categoriesList";
 import Species from "../components/layout/speciesList";
+import ServicesList from "../components/layout/servicesList";
 
 function Home() {
+  const isMediumScreen = useMediaQuery("(max-width: 960px)"); // Verifica se a tela é de tamanho médio ou menor
+
   return (
     <div className="container">
-      <Box sx={{marginTop: "50px", paddingBottom: "50px"}}>
+      <Box sx={{ marginTop: "50px", paddingBottom: "50px" }}>
         <SearchBar />
         <BoxWelcome />
-        <Categories />
-        <Species />
-        {/* */}
+        {/* Renderiza os componentes apenas se não estiver em tamanho médio */}
+        {!isMediumScreen && (
+          <>
+            <Categories />
+            <Species />
+            <ServicesList />
+          </>
+        )}
       </Box>
     </div>
   );
