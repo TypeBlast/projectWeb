139868dce0f84bc243a29dd69aad6436fcb07286
igordmouraa <igordmoura_@hotmail.com>
2024-10-01 23:56:53 -0300

    fix(routes): fix admin route 50%

--- src/components/utils/protectroute.jsx
+++ src/components/utils/protectroute.jsx
@@ -1,10 +1,28 @@
 import { Navigate } from "react-router-dom";
 
 function ProtectedRoute({ children }) {
-  const token = localStorage.getItem("token"); // Pegar o token do localStorage
+  const user = JSON.parse(localStorage.getItem("user"));
+  const token = localStorage.getItem("token");
 
-  // Se o token não existir, redirecione para a página de erro
-  return token ? children : <Navigate to="/error" />;
+  // Verifica se o token existe
+  if (!token) {
+    return <Navigate to="/error" />;
+  }
+
+  // Obtém a role do usuário
+  const role = user?.data?.role; 
+
+  // Verifica se a rota requer acesso administrativo
+  const adminRoutes = ["/admin"];
+  const requiresAdmin = adminRoutes.includes(window.location.pathname);
+
+  // Se a rota requer acesso admin e o usuário não é admin, redireciona para erro
+  if (requiresAdmin && role !== "admin") {
+    return <Navigate to="/error" />;
+  }
+
+  // Se for admin ou a rota não requer admin, permite acesso
+  return children;
 }
 
 export default ProtectedRoute;
