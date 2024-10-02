348457b39904e8ccc7fbcd9af86cd8c7f93a413f
igordmouraa <igordmoura_@hotmail.com>
2024-10-01 21:01:35 -0300

    feat(address): add local pin on address box

--- src/components/layout/boxAddressData.jsx
+++ src/components/layout/boxAddressData.jsx
@@ -1,7 +1,12 @@
 import React, { useState, useEffect } from "react";
 import { Typography, Box, Grid, Button, Modal } from "@mui/material";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPen, faXmark, faTrash } from "@fortawesome/free-solid-svg-icons";
+import {
+  faPen,
+  faXmark,
+  faTrash,
+  faLocationDot,
+} from "@fortawesome/free-solid-svg-icons";
 import axios from "../../axios/axios";
 import InputComplement from "../inputs/inputComplement";
 import InputNumber from "../inputs/inputNumber";
@@ -104,7 +109,13 @@ function BoxAddress() {
 
   const handleSave = async () => {
     try {
-      const addressData = { complement, number, cep, state_id: stateId, city_id: cityId };
+      const addressData = {
+        complement,
+        number,
+        cep,
+        state_id: stateId,
+        city_id: cityId,
+      };
 
       if (!number || !cep || !stateId || !cityId) {
         setError("Todos os campos obrigatórios devem ser preenchidos.");
@@ -122,10 +133,13 @@ function BoxAddress() {
       // Atualiza os endereços após a edição ou adição
       const response = await axios.getAddressByUser();
       setAddresses(response.data.data);
-      
+
       handleClose();
     } catch (error) {
-      console.error("Erro ao salvar endereço:", error.response?.data || error.message);
+      console.error(
+        "Erro ao salvar endereço:",
+        error.response?.data || error.message
+      );
       setError(error.response?.data?.message || "Erro ao realizar login.");
     }
   };
@@ -134,14 +148,17 @@ function BoxAddress() {
     try {
       await axios.deleteAddress(addressId);
       setSuccess("Endereço deletado com sucesso!");
-      
+
       // Atualiza os endereços após a deleção
       const response = await axios.getAddressByUser();
       setAddresses(response.data.data);
-      
+
       setConfirmDeleteOpen(false);
     } catch (error) {
-      console.error("Erro ao deletar endereço:", error.response?.data || error.message);
+      console.error(
+        "Erro ao deletar endereço:",
+        error.response?.data || error.message
+      );
       setError("Erro ao deletar endereço. Tente novamente.");
       setConfirmDeleteOpen(false);
     }
@@ -160,7 +177,10 @@ function BoxAddress() {
             boxShadow: "0px 4px 4px rgba(191, 191, 191, 0.75)",
           }}
         >
-          <Typography variant="h6" sx={{ fontFamily: "Poppins-Bold", marginBottom: "20px" }}>
+          <Typography
+            variant="h6"
+            sx={{ fontFamily: "Poppins-Bold", marginBottom: "20px" }}
+          >
             Endereços Salvos
           </Typography>
           {addresses.length === 0 ? (
@@ -168,26 +188,56 @@ function BoxAddress() {
           ) : (
             addresses.map((address) => (
               <Grid container key={address.id} sx={{ marginBottom: "10px" }}>
-                <Grid item xs={8}>
-                  <Typography>{`${address.complement}, ${address.number}, ${address.cep}`}</Typography>
+                <Grid
+                  item
+                  xs={8}
+                  sx={{ display: "flex", alignItems: "center" }}
+                >
+                  {/* Contêiner flexível para alinhar o ícone e o texto */}
+                  <Box sx={{ display: "flex", alignItems: "center" }}>
+                    <FontAwesomeIcon
+                      icon={faLocationDot}
+                      style={{ marginRight: "8px", color: "#D9D9D9" }}
+                    />
+                    <Typography>{`${address.complement}, ${address.number}, ${address.cep}`}</Typography>
+                  </Box>
                 </Grid>
-                <Grid item xs={4} sx={{ display: "flex", justifyContent: "flex-end" }}>
-                  <Button onClick={() => handleEditAddress(address)} sx={{
+                <Grid
+                  item
+                  xs={4}
+                  sx={{ display: "flex", justifyContent: "flex-end" }}
+                >
+                  <Button
+                    onClick={() => handleEditAddress(address)}
+                    sx={{
                       backgroundColor: "transparent",
-                      '&:hover': {
+                      "&:hover": {
                         backgroundColor: "#D9D9D9",
                       },
-                    }}>
-                    <FontAwesomeIcon icon={faPen} style={{ fontSize: "1rem", color: "#D9D9D9" }} />
+                    }}
+                  >
+                    <FontAwesomeIcon
+                      icon={faPen}
+                      style={{ fontSize: "1rem", color: "#D9D9D9" }}
+                    />
                   </Button>
-                  <Button onClick={() => { setCurrentAddress(address); setConfirmDeleteOpen(true); }} sx={{
+                  <Button
+                    onClick={() => {
+                      setCurrentAddress(address);
+                      setConfirmDeleteOpen(true);
+                    }}
+                    sx={{
                       marginLeft: "10px",
                       backgroundColor: "transparent",
-                      '&:hover': {
+                      "&:hover": {
                         backgroundColor: "#D9D9D9",
                       },
-                    }}>
-                    <FontAwesomeIcon icon={faTrash} style={{ fontSize: "1rem", color: "#D9D9D9" }} />
+                    }}
+                  >
+                    <FontAwesomeIcon
+                      icon={faTrash}
+                      style={{ fontSize: "1rem", color: "#D9D9D9" }}
+                    />
                   </Button>
                 </Grid>
               </Grid>
@@ -205,7 +255,7 @@ function BoxAddress() {
               color: "#FFF",
               textTransform: "capitalize",
               fontSize: "1rem",
-              '&:hover': {
+              "&:hover": {
                 backgroundColor: "#D5006D", // Cor ao passar o mouse
               },
             }}
@@ -218,21 +268,66 @@ function BoxAddress() {
       {/* Modal para Adicionar/Editar Endereço */}
       <Modal open={open} onClose={handleClose}>
         <Box sx={modalStyle}>
-          <Button onClick={handleClose} sx={{ position: "absolute", top: 10, right: 25, minWidth: "auto" }}>
-            <FontAwesomeIcon icon={faXmark} style={{ color: "#BFBFBF", fontSize: "1.5rem" }} />
+          <Button
+            onClick={handleClose}
+            sx={{ position: "absolute", top: 10, right: 25, minWidth: "auto" }}
+          >
+            <FontAwesomeIcon
+              icon={faXmark}
+              style={{ color: "#BFBFBF", fontSize: "1.5rem" }}
+            />
           </Button>
-          <Typography sx={{ fontFamily: "Poppins-Bold", fontSize: "1.3rem", marginTop: "30px", textAlign: "center" }}>
+          <Typography
+            sx={{
+              fontFamily: "Poppins-Bold",
+              fontSize: "1.3rem",
+              marginTop: "30px",
+              textAlign: "center",
+            }}
+          >
             {editMode ? "Editar Endereço" : "Adicionar Endereço"}
           </Typography>
-          {error && <Typography sx={{ color: "red", textAlign: "center" }}>{error}</Typography>}
-          {success && <Typography sx={{ color: "green", textAlign: "center" }}>{success}</Typography>}
-          <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', width: '100%' }}>
-            <InputComplement value={complement} onChange={(e) => setComplement(e.target.value)} />
-            <InputNumber value={number} onChange={(e) => setNumber(e.target.value)} />
+          {error && (
+            <Typography sx={{ color: "red", textAlign: "center" }}>
+              {error}
+            </Typography>
+          )}
+          {success && (
+            <Typography sx={{ color: "green", textAlign: "center" }}>
+              {success}
+            </Typography>
+          )}
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              width: "100%",
+            }}
+          >
+            <InputComplement
+              value={complement}
+              onChange={(e) => setComplement(e.target.value)}
+            />
+            <InputNumber
+              value={number}
+              onChange={(e) => setNumber(e.target.value)}
+            />
             <InputCep value={cep} onChange={(e) => setCep(e.target.value)} />
-            <InputState states={states} value={stateId} onChange={(e) => setStateId(e.target.value)} />
-            <InputCity cities={cities} value={cityId} onChange={(e) => setCityId(e.target.value)} />
-            <Button onClick={handleSave} variant="contained" sx={{
+            <InputState
+              states={states}
+              value={stateId}
+              onChange={(e) => setStateId(e.target.value)}
+            />
+            <InputCity
+              cities={cities}
+              value={cityId}
+              onChange={(e) => setCityId(e.target.value)}
+            />
+            <Button
+              onClick={handleSave}
+              variant="contained"
+              sx={{
                 backgroundColor: "#EB389A",
                 color: "#FFF",
                 textTransform: "capitalize",
@@ -240,10 +335,11 @@ function BoxAddress() {
                 marginTop: "20px",
                 width: "100%",
                 fontFamily: "Poppins-Bold",
-                '&:hover': {
+                "&:hover": {
                   backgroundColor: "#D5006D",
                 },
-              }}>
+              }}
+            >
               Salvar
             </Button>
           </Box>
@@ -251,32 +347,50 @@ function BoxAddress() {
       </Modal>
 
       {/* Modal de confirmação de exclusão */}
-      <Modal open={confirmDeleteOpen} onClose={() => setConfirmDeleteOpen(false)}>
+      <Modal
+        open={confirmDeleteOpen}
+        onClose={() => setConfirmDeleteOpen(false)}
+      >
         <Box sx={modalStyle}>
-          <Typography sx={{ fontFamily: "Poppins-Bold", fontSize: "1.3rem", textAlign: "center", marginBottom: "20px" }}>
+          <Typography
+            sx={{
+              fontFamily: "Poppins-Bold",
+              fontSize: "1.3rem",
+              textAlign: "center",
+              marginBottom: "20px",
+            }}
+          >
             Tem certeza que deseja deletar este endereço?
           </Typography>
-          <Box sx={{ display: 'flex', justifyContent: 'space-around' }}>
-            <Button onClick={() => handleDeleteAddress(currentAddress.id)} variant="contained" sx={{
+          <Box sx={{ display: "flex", justifyContent: "space-around" }}>
+            <Button
+              onClick={() => handleDeleteAddress(currentAddress.id)}
+              variant="contained"
+              sx={{
                 backgroundColor: "#EB389A",
                 color: "#FFF",
                 textTransform: "capitalize",
                 fontSize: "1rem",
-                '&:hover': {
+                "&:hover": {
                   backgroundColor: "#D5006D",
                 },
-              }}>
+              }}
+            >
               Sim
             </Button>
-            <Button onClick={() => setConfirmDeleteOpen(false)} variant="outlined" sx={{
+            <Button
+              onClick={() => setConfirmDeleteOpen(false)}
+              variant="outlined"
+              sx={{
                 color: "#EB389A",
                 textTransform: "capitalize",
                 fontSize: "1rem",
                 borderColor: "#EB389A",
-                '&:hover': {
+                "&:hover": {
                   borderColor: "#D5006D",
                 },
-              }}>
+              }}
+            >
               Não
             </Button>
           </Box>
