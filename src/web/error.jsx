f27377dd4ea20c5ef25ba8bf3ec6c76e516e7fc5
dePaula06 <p.depaula06.sp@gmail.com>
2024-10-22 15:23:26 -0300

    chore(error): Fixed responsiveness bugs on the Error page

--- src/web/error.jsx
+++ src/web/error.jsx
@@ -2,25 +2,30 @@ import React from "react";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import ImageHome from "../assets/images/imageHome.png";
-
+import { useMediaQuery } from "@mui/material"; // Importar o hook
 import { useNavigate } from "react-router-dom";
 
 function Error() {
   const navigate = useNavigate();
+  const isMediumOrSmallerScreen = useMediaQuery("(max-width: 960px)"); // Verifica se a tela é 'md' ou menor
+
   const handleNavigation = (path) => {
     navigate(path); // Chama o hook de navegação
   };
+
   return (
     <div className="container">
-      <img
-        src={ImageHome}
-        style={{
-          width: 350,
-          height: 300,
-          marginTop: 85,
-        }}
-        alt="Error"
-      />
+      {!isMediumOrSmallerScreen && ( // Renderiza a imagem apenas se a tela for maior que 'md'
+        <img
+          src={ImageHome}
+          style={{
+            width: 350,
+            height: 300,
+            marginTop: 85,
+          }}
+          alt="Error"
+        />
+      )}
       <Typography className="textError">
         Cadastre-se ou faça login para acessar nossos serviços exclusivos e
         cuidar do seu pet com facilidade!
