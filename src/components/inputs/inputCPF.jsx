084d5108dda7e83b47ce9afc8fa41dd2e1400956
dePaula06 <p.depaula06.sp@gmail.com>
2024-10-29 15:38:56 -0300

    chore(inputCPF): Adding a CPF mask, using the InputMask library

--- src/components/inputs/inputCPF.jsx
+++ src/components/inputs/inputCPF.jsx
@@ -1,41 +1,42 @@
 import React from "react";
-
+import InputMask from "react-input-mask"; // Importa a biblioteca
 import { TextField, Box } from "@mui/material";
-
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faIdCard } from '@fortawesome/free-solid-svg-icons';
 
 function InputCPF({ value, onChange }) {
+  const handleChange = (event) => {
+    const newValue = event.target.value.replace(/\D/g, ''); // Remove tudo que não for dígito
+    onChange({ target: { value: newValue } }); // Passa apenas os dígitos para o onChange
+  };
+
   return (
     <Box sx={{ display: 'flex', alignItems: 'center' }}>
-    <FontAwesomeIcon icon={faIdCard} style={{ marginRight: '10px', marginTop: "20px", fontSize: '20px', color: "#D9D9D9" }} />
-    <TextField
-      id="cpf"
-      label="CPF"
-      variant="standard"
-      value={value} // Define o valor do input
-      onChange={onChange} // Define a função de alteração
-      sx={{
-        margin: "5px",
-        width: '220px', // Controla o tamanho
-        '& label': {
-          color: '#D9D9D9', // Cor do label
-        },
-        '& label.Mui-focused': {
-          color: '#A8A8A8', // Cor do label quando focado
-        },
-        '& .MuiInput-underline:before': {
-          borderBottomColor: '#D9D9D9', // Cor da linha inferior antes do foco
-        },
-        '& .MuiInput-underline:after': {
-          borderBottomColor: '#A8A8A8', // Cor da linha inferior quando focado
-        },
-        '& .MuiInputBase-input': {
-          color: '#333', // Cor do texto digitado
-        },
-      }}
-    />
-  </Box>
+      <FontAwesomeIcon icon={faIdCard} style={{ marginRight: '10px', marginTop: "20px", fontSize: '20px', color: "#D9D9D9" }} />
+      <InputMask
+        mask="999.999.999-99" // Máscara de CPF
+        value={value}
+        onChange={handleChange}
+        style={{ border: 'none' }}
+      >
+        {() => (
+          <TextField
+            id="cpf"
+            label="CPF"
+            variant="standard"
+            sx={{
+              margin: "5px",
+              width: '220px',
+              '& label': { color: '#D9D9D9' },
+              '& label.Mui-focused': { color: '#A8A8A8' },
+              '& .MuiInput-underline:before': { borderBottomColor: '#D9D9D9' },
+              '& .MuiInput-underline:after': { borderBottomColor: '#A8A8A8' },
+              '& .MuiInputBase-input': { color: '#333' },
+            }}
+          />
+        )}
+      </InputMask>
+    </Box>
   );
 }
 
