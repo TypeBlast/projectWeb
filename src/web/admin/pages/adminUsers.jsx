877b21f8230a6d941443380a84a9d8e127cb9d5c
Isadora Chieregato <chieregatoisadora@gmail.com>
2024-11-05 18:26:41 -0300

    fix(adminUsers):creation function EditUser

--- src/web/admin/pages/adminUsers.jsx
+++ src/web/admin/pages/adminUsers.jsx
@@ -2,12 +2,20 @@ import React, { useEffect, useState } from "react";
 import { Box, Grid, Typography, Button, Modal } from "@mui/material";
 import axios from "../../../axios/axios";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"; 
-import { faTrash } from "@fortawesome/free-solid-svg-icons";
+import { faTrash, faPen } from "@fortawesome/free-solid-svg-icons";
+import InputName from "../../../components/inputs/inputName";
+import InputEmail from "../../../components/inputs/inputEmail";
+import InputPhone from "../../../components/inputs/inputPhone";
 
 function AdminUsers() {
   const [users, setUsers] = useState([]);
   const [openConfirmModal, setOpenConfirmModal] = useState(false);
   const [selectedUserId, setSelectedUserId] = useState(null);
+  const [selectedUser, setSelectedUser] = useState(null);
+  const [openModal, setOpenModal] = useState(false);
+  const [updatedName, setUpdatedName] = useState("");
+  const [updatedPhone, setUpdatedPhone] = useState("");
+  const [updatedEmail, setUpdatedEmail] = useState("");
 
   const fetchUsers = async () => {
     try {
@@ -16,7 +24,28 @@ function AdminUsers() {
       setUsers(usersData);
     } catch (error) {
       console.error("Erro ao buscar usuários:", error);
-      setUsers([]);
+    }
+  };
+
+  const handleEditUser = (user) => {
+    setSelectedUser(user);
+    setUpdatedName(user.name);
+    setUpdatedPhone(user.phone);
+    setUpdatedEmail(user.email);
+    setOpenModal(true);
+  };
+
+  const handleUpdateUser = async () => {
+    try {
+      await axios.putUser({
+        name: updatedName,
+        phone: updatedPhone,
+        email: updatedEmail,
+      }, selectedUser.id); 
+      setOpenModal(false);
+      fetchUsers();
+    } catch (error) {
+      console.error("Erro ao atualizar usuário:", error);
     }
   };
 
@@ -35,6 +64,12 @@ function AdminUsers() {
   const handleClickOpen = (userId) => {
     setSelectedUserId(userId);
     setOpenConfirmModal(true);
+  };
+
+  
+  const handleCloseModal = () => {
+    setOpenModal(false);
+    setSelectedUser(null);
   };
 
   const handleCloseConfirmModal = () => {
@@ -69,7 +104,6 @@ function AdminUsers() {
             Todos os Usuários
           </Typography>
 
-          {/* Cabeçalho */}
           <Grid container sx={{ marginBottom: "10px", borderBottom: "2px solid #000", paddingBottom: "10px" }}>
             <Grid item xs={12} sm={4}>
               <Typography variant="subtitle1" sx={{ fontFamily: "Poppins-Bold" }}>Nome</Typography>
@@ -85,10 +119,23 @@ function AdminUsers() {
                 <Grid item xs={12} sm={4}>
                   <Typography sx={{ fontFamily: "Poppins-Regular" }}>{user.name}</Typography>
                 </Grid>
-                <Grid item xs={12} sm={6}>
+                <Grid item xs={12} sm={5}>
                   <Typography sx={{ fontFamily: "Poppins-Regular", color: "text.secondary" }}>{user.email}</Typography>
                 </Grid>
-                <Grid item xs={12} sm={2} sx={{ display: "flex", justifyContent: "flex-end" }}>
+                <Grid item xs={12} sm={3} sx={{ display: "flex", justifyContent: "flex-end" }}>
+                  
+                <Button onClick={() => handleEditUser(user)} 
+                sx={{
+                   backgroundColor: "#EB389A",
+                    color: "#FFF",
+                     textTransform: "capitalize", 
+                     fontSize: "1rem",
+                      "&:hover": { 
+                        backgroundColor: "#D5006D"
+                       },
+                         }}>
+                    <FontAwesomeIcon icon={faPen} />
+                  </Button>
                   <Button
                     onClick={() => handleClickOpen(user.id)} 
                     sx={{
@@ -96,6 +143,7 @@ function AdminUsers() {
                       color: "#FFF",
                       textTransform: "capitalize",
                       fontSize: "1rem",
+                      marginLeft:"10px",
                       "&:hover": {
                         backgroundColor: "#D5006D",
                       },
@@ -114,6 +162,43 @@ function AdminUsers() {
         </Box>
       </Grid>
 
+        {/* Modal de Edição de Funcionário */}
+        <Modal open={openModal} onClose={handleCloseModal}>
+        <Box sx={{ width: 400, bgcolor: "background.paper", borderRadius: "8px", boxShadow: 24, p: 4, margin: "auto", marginTop: "100px" }}>
+          <Typography variant="h6" component="h2" sx={{ textAlign: "center", marginBottom: "20px" }}>Editar Usuário</Typography>
+          <InputName value={updatedName} onChange={(e) => setUpdatedName(e.target.value)} />
+          <InputPhone value={updatedPhone} onChange={(e) => setUpdatedPhone(e.target.value)} />
+          <InputEmail value={updatedEmail} onChange={(e) => setUpdatedEmail(e.target.value)} />
+         
+
+          <Box sx={{ display: "flex", flexDirection: "row", alignItems: "center", gap: "20px" }}>
+            <Button onClick={handleCloseModal} sx={{ width: "250px",
+                backgroundColor: "#EB389A",
+                marginTop: "20px",
+                fontFamily: "Poppins-Bold",
+                color: "#FFF",
+                textTransform: "capitalize",
+                fontSize: "1rem",
+                "&:hover": {
+                  backgroundColor: "#D5006D",} }}>
+              Cancelar
+            </Button>
+            <Button onClick={handleUpdateUser} sx={{ width: "250px",
+                backgroundColor: "#EB389A",
+                marginTop: "20px",
+                fontFamily: "Poppins-Bold",
+                color: "#FFF",
+                textTransform: "capitalize",
+                fontSize: "1rem",
+                "&:hover": {
+                  backgroundColor: "#D5006D",} }}>
+              Salvar
+            </Button>
+          </Box>
+        </Box>
+      </Modal>
+
+      {/* Modal de Confirmação de Exclusão */}
       <Modal
         open={openConfirmModal}
         onClose={handleCloseConfirmModal}
